b0bfabc842310220cd2bbb8416814965354eeca2
Kosawat Sukchaya <kosawat@gmail.com>
2025-06-16 16:40:37 +0200

    Implemented mobile menu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from "@/components/nav/mobile-nav";
 import SideNav from "@/components/nav/side-nav";
 import React from "react";
 
@@ -8,11 +9,16 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="flex h-screen">
-      <div className="w-1/4 h-full">
+      <div className="hidden md:block w-1/4 h-full">
         <SideNav />
       </div>
-      <div className="flex-1 overflow-y-auto">
-        <div className="m-4">{children}</div>
+      <div className="flex flex-col md:flex-row flex-1 w-full">
+        <div className="md:hidden w-full">
+          <MobileNav />
+        </div>
+        <div className="flex-1 overflow-y-auto">
+          <div className="m-4">{children}</div>
+        </div>
       </div>
     </div>
   );
